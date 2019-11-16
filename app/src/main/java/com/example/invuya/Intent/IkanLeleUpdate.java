887528e4fc6afd647baaf6a8ee8185fad742c346
taufiq.ivan15@student.untan.ac.id <taufiq.ivan15@student.untan.ac.id>
2019-11-16 14:58:30 +0700

    update

---
+++ app/src/main/java/com/example/invuya/Intent/IkanLeleUpdate.java
@@ -0,0 +1,16 @@
+package com.example.invuya.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.invuya.R;
+
+public class IkanLeleUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ikan_lele_update);
+    }
+}
